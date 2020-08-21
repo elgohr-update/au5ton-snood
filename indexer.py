ad773e0835cc58bb8b16e97cc81e62dbb68cea07
Austin Jackson <themacphage@gmail.com>
2020-08-20 22:21:59 -0500

    update

--- indexer.py
+++ indexer.py
@@ -2,9 +2,11 @@
 
 import os
 import praw
+import praw.models
 import configparser
 import sqlite3
 import time
+from datetime import date
 
 from snood.ui import Spinner
 import snood.util
@@ -12,79 +14,98 @@ from tqdm import tqdm
 import humanfriendly
 from signal import signal, SIGINT
 
-def handler(signal_received, frame):
-    # Handle any cleanup here
-    print('SIGINT or CTRL-C detected. Exiting gracefully')
-    exit(0)
-    #os.system(f'kill {os.getpid()}')
-
-signal(SIGINT, handler)
+def exists_already(post: praw.models.Submission, conn: sqlite3.Connection) -> bool:
+  c = conn.cursor()
+  c.execute(f'SELECT COUNT(id) FROM reddit_submissions WHERE id=?', (post.id,))
+  count = c.fetchall()[0]['COUNT(id)']
+  if(count > 0):
+    return True
+  else:
+    return False
+  
+def localize_utc(x: int) -> str:
+  return date.fromtimestamp(x).strftime('%b %d, %Y')
 
 program_execute_time = time.mktime(time.localtime())
 
 config = configparser.ConfigParser()
 # reads config.ini that's adjacent to main.py
-config.read(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.ini'))
+config.read(os.path.join(os.path.dirname(
+  os.path.abspath(__file__)), 'config.ini'))
 
 reddit = praw.Reddit(client_id=config['REDDIT']['ClientId'],
-                     client_secret=config['REDDIT']['ClientSecret'],
-                     password=config['REDDIT']['Password'], user_agent='au5ton/snood',
-                     username=config['REDDIT']['Username'])
+           client_secret=config['REDDIT']['ClientSecret'],
+           password=config['REDDIT']['Password'], user_agent='au5ton/snood',
+           username=config['REDDIT']['Username'])
 
 friends = []
 with Spinner('Loading friends from reddit'):
-    friends = reddit.user.friends()
+  friends = reddit.user.friends()
 
 conn = sqlite3.connect('database.sqlite')
 conn.row_factory = snood.util.dict_factory
 c = conn.cursor()
 
 with Spinner('Initializing database'):
-    # PRAW should have this information lazy-loaded and follow-up requests shouldn't be necessary
-    # https://praw.readthedocs.io/en/v3.6.0/pages/lazy-loading.html
-    c.execute('''
-    CREATE TABLE IF NOT EXISTS redditors (
-        name text unique,
-        id text unique,
-        snood_added_utc integer
-        )
-    ''')
+  # PRAW should have this information lazy-loaded and follow-up requests shouldn't be necessary
+  # https://praw.readthedocs.io/en/v3.6.0/pages/lazy-loading.html
+  c.execute('''
+  CREATE TABLE IF NOT EXISTS redditors (
+  name text unique,
+  id text unique,
+  snood_added_utc real
+  )
+  ''')
 
-    c.execute('''
-    CREATE TABLE IF NOT EXISTS reddit_submissions (
-        title text,
-        author text,
-        created_utc integer,
-        permalink text,
-        url text,
-        id text unique,
-        num_comments integer,
-        score integer,
-        snood_added_utc integer,
-        snood_downloaded boolean
-        )
-    ''')
+  c.execute('''
+  CREATE TABLE IF NOT EXISTS reddit_submissions (
+  title text,
+  author text,
+  created_utc real,
+  permalink text,
+  url text,
+  id text unique,
+  num_comments integer,
+  score integer,
+  snood_added_utc real,
+  snood_downloaded boolean
+  )
+  ''')
 
 with Spinner('Populating database with redditor information'):
-    for redditor in friends:
-        c.execute(f'INSERT OR IGNORE INTO redditors VALUES {str((redditor.name, redditor.id, time.mktime(time.localtime())))}')
-        conn.commit()
+  for redditor in friends:
+    c.execute(
+      f'INSERT OR IGNORE INTO redditors VALUES {str((redditor.name, redditor.id, time.mktime(time.localtime())))}')
+    conn.commit()
 
 # only usernames are necessary
 friends = [u.name for u in friends]
 print(f'Downloading post history')
 with tqdm(total=len(friends), unit='user') as pbar:
-    for username in friends:
-        pbar.set_description(username)
-        try:
-            for post in tqdm(reddit.redditor(username).submissions.new(limit=None), total=1000, unit='post', leave=False):
-                if post.is_self == False:
-                    c.execute(f'INSERT OR IGNORE INTO reddit_submissions VALUES (?,?,?,?,?,?,?,?,?,?)', (post.title, post.author.name, int(post.created_utc), f'https://reddit.com{post.permalink}', post.url, post.id, int(post.num_comments), int(post.score), time.mktime(time.localtime()), False))
-                    conn.commit()
-        except:
-            # user probably deleted
-            pass
-        pbar.update()
+  for username in friends:
+    pbar.set_description(username)
+    new_post_count = 0
+    message = ''
+    try:
+      for post in reddit.redditor(username).submissions.new(limit=None):
+        if post.is_self == False:
+          exists = exists_already(post, conn)
+          if(exists == False):
+            new_post_count += 1
+          if(post.stickied or not exists):
+            c.execute(f'INSERT OR IGNORE INTO reddit_submissions VALUES (?,?,?,?,?,?,?,?,?,?)', (post.title, post.author.name, post.created_utc, f'https://reddit.com{post.permalink}', post.url, post.id, int(post.num_comments), int(post.score), time.mktime(time.localtime()), False))
+            conn.commit()
+          message = f'\t{post.author.name} had {new_post_count} new posts. Already indexed up to {localize_utc(post.created)}.'
+          if(exists and not post.stickied):
+            break
+      pbar.write(message)
+    except KeyboardInterrupt:
+      exit(0)
+    except Exception as err:
+      # user probably deleted
+      print('Unexpected error:', err)
+      pass
+    pbar.update()
 
 seconds_passed = time.mktime(time.localtime()) - program_execute_time
 print(f'Program took {humanfriendly.format_timespan(seconds_passed)} to complete.')
--- ripper.py
+++ ripper.py
@@ -12,13 +12,13 @@ from tqdm import tqdm
 import humanfriendly
 from signal import signal, SIGINT
 
-def handler(signal_received, frame):
-    # Handle any cleanup here
-    print('SIGINT or CTRL-C detected. Exiting gracefully')
-    exit(0)
-    #os.system(f'kill {os.getpid()}')
+# def handler(signal_received, frame):
+#   # Handle any cleanup here
+#   print('SIGINT or CTRL-C detected. Exiting gracefully')
+#   exit(0)
+#   #os.system(f'kill {os.getpid()}')
 
-signal(SIGINT, handler)
+#signal(SIGINT, handler)
 
 program_execute_time = time.mktime(time.localtime())
 
@@ -34,18 +34,21 @@ c = conn.cursor()
 submissions = []
 
 with Spinner('Querying database for posts to traverse'):
-    c.execute('SELECT * FROM reddit_submissions WHERE snood_downloaded = 0 ORDER BY author, created_utc ASC')
-    submissions = c.fetchall()
+  c.execute('SELECT * FROM reddit_submissions WHERE snood_downloaded = 0 ORDER BY author, created_utc ASC')
+  submissions = c.fetchall()
 
 print(f'{len(submissions)} posts queued to process')
 
 with tqdm(total=len(submissions), unit='posts') as pbar:
-    for post in submissions:
-        pbar.set_description(f'/u/{post["author"]} https://redd.it/{post["id"]}')
-        snood.downloader.download(os.path.join(DOWNLOAD_DIR, post['author']), post['url'])
-        c.execute('UPDATE reddit_submissions SET snood_downloaded = 1 WHERE id = ?', (post["id"],))
-        conn.commit()
-        pbar.update()
+  for post in submissions:
+    try:
+      pbar.set_description(f'/u/{post["author"]} https://redd.it/{post["id"]}')
+      snood.downloader.download(os.path.join(DOWNLOAD_DIR, post['author']), post['url'])
+      c.execute('UPDATE reddit_submissions SET snood_downloaded = 1 WHERE id = ?', (post["id"],))
+      conn.commit()
+      pbar.update()
+    except KeyboardInterrupt:
+      exit(0)
 
 seconds_passed = time.mktime(time.localtime()) - program_execute_time
 print(f'Program took {humanfriendly.format_timespan(seconds_passed)} to complete.')
